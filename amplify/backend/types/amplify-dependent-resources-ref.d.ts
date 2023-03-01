c5b0f5d929c424e7f039b79cb70b733367ad768d
josefaidt <josef.aidt@gmail.com>
2023-03-01 12:55:17 -0800

    post push

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,5 +1,12 @@
 export type AmplifyDependentResourcesAttributes = {
   "function": {
+    "a1211408a68fe6": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "a1211486eff962": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
